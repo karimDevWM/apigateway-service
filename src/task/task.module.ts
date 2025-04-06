86135c1998492d8e4856714e9daaa7963f920da0
devjava69 <k.trabelsi9@laposte.net>
2025-04-06 23:04:37 +0200

    update file path for natsclientmodule

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
-import { NatsClientModule } from 'src/nats-client/nats-client.module';
+import { NatsClientModule } from '../nats-client/nats-client.module';
 
 @Module({
   imports: [NatsClientModule],
